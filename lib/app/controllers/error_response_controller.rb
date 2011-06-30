29ddc49aff72a212bc86566ec6bf842618ba3e2f
Bjørn Trondsen <bjorn@arctodus.no>
2011-06-30 19:33:51 +0200

    the failed controller actions default layout are now used as the error response layout

--- lib/app/controllers/error_response_controller.rb
+++ lib/app/controllers/error_response_controller.rb
@@ -1,5 +1,9 @@
+# encoding: utf-8
 class ErrorResponseController < ActionController::Base
   def index
-    render(:text => "<h1>Det oppsto en feil</h1><p>Feilmeldingen har blitt logget</p>", :layout => 'layouts/application')
+    html = "<h1>Det oppsto en feil</h1>
+            <p>Feilmeldingen har blitt logget sånn at den kan brukes til å løse eventuelle feil i applikasjonen.
+            Hvis problemet vedvarer så kan du sende en e-post til <a href='mailto:railsadmin@bitsenter.no'>railsadmin@bitsenter.no</a></p>"
+    render(:text => html, :layout => @_env['layout_for_exception_response'])
   end
 end
