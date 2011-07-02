f1b47c689aa9ced5d1b3e56692852911554d3b3e
Sharagoz <contact@sharagoz.com>
2011-07-02 17:05:30 +0200

    added test for controller action and data stored

--- test/exception_handler_test.rb
+++ test/exception_handler_test.rb
@@ -16,9 +16,19 @@ class ExceptionHandlerTest < Test::Unit::TestCase
     Rails.application.app
   end
 
-  def test_catches_rails_exceptions
+  def test_catches_controller_errors
     get "/home/action_with_error"
-    assert_equal("An error has occurred", last_response.body)
+    assert(last_response.body.match(/An error has occurred/), 'incorrect response')
+    assert(ErrorMessage.count == 1, "message count should be 1, but was #{ErrorMessage.count}")
+    ErrorMessage.destroy_all
+  end
+
+  def test_stores_correct_information
+    get "/home/action_with_error"
+    assert(ErrorMessage.count == 1, "message count should be 1, but was #{ErrorMessage.count}")
+    msg = ErrorMessage.first
+    assert(msg.app_name == 'ExceptionHandlerTestApp', 'app_name field is wrong')
+    ErrorMessage.destroy_all
   end
 
 end
