1b0cae2254ced79f0a28bb6dcf95b84c5e59ec38
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-03 19:58:54 +0200

    create_parser macro added

--- spec/test_macros.rb
+++ spec/test_macros.rb
@@ -8,7 +8,15 @@ module TestMacros
 
   def create_env
     s = Rack::Test::Session.new(nil)
-    env = s.send(:env_for,'/home/', {:params => {:foo => 'bar'}, 'HTTP_REFERER' => 'http://google.com/', 'HTTP_USER_AGENT' => "Mozilla/4.0 (compatible; MSIE 8.0)"})
+    env = s.send(:env_for,'/home', {:params => {:foo => 'bar'}, 'HTTP_REFERER' => 'http://google.com/', 'HTTP_USER_AGENT' => "Mozilla/4.0 (compatible; MSIE 8.0)"})
+  end
+
+  def create_parser(exception = nil, request = nil, controller = nil)
+      env = create_env
+      controller ||= mock(ApplicationController, :current_user => mock(Object, :login => 'superman'))
+      request ||= ActionDispatch::Request.new(env)
+      exception ||= create_exception
+      parser = RailsExceptionHandler::Parser.new(exception, request, controller)
   end
 
   def create_exception
