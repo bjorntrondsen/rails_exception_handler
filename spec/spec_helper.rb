e232744b58a06683c733b51e636d35cedfa55c1d
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-03 16:56:41 +0200

    test log macros added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,22 @@
 
 ENV["RAILS_ENV"] = 'test'
 require File.expand_path(File.dirname(__FILE__)) + '/exception_handler_test_app/config/environment.rb'
+
 require File.expand_path(File.dirname(__FILE__)) + '/test_macros.rb'
 
 require "rspec/rails"
 require "rack/test"
 
+
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.include TestMacros
   config.around do |example|
+    clear_test_log
     ErrorMessage.delete_all
     example.call
     ErrorMessage.delete_all
+    clear_test_log
   end
   config.color_enabled = true
   config.full_backtrace = true
