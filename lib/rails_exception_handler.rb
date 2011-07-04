7d0a60d4491d6664a57040e4c2a2a9e211d1cb1c
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-04 21:30:16 +0200

    added proper initializer

--- lib/rails_exception_handler.rb
+++ lib/rails_exception_handler.rb
@@ -8,4 +8,18 @@ class RailsExceptionHandler
   rescue Exception => e
     Handler.new(env, e).handle_exception
   end
+
+  def self.configure
+    #if Rails.env == 'production'
+    require File.expand_path(File.dirname(__FILE__)) + '/patch/show_exceptions.rb'
+    %w{ models controllers }.each do |dir|
+      path = File.join(File.dirname(__FILE__), 'app', dir)
+      $LOAD_PATH << path
+      ActiveSupport::Dependencies.autoload_paths << path
+      ActiveSupport::Dependencies.autoload_once_paths.delete(path)
+    end
+    Rails.configuration.middleware.use RailsExceptionHandler
+    Rails.configuration.action_dispatch.show_exceptions = true
+    #end
+  end
 end
