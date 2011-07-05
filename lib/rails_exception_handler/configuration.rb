f4667b795ac2ee645604e0827dd4c84693853aac
Sharagoz <contact@sharagoz.com>
2011-07-05 20:43:53 +0200

    enviroments changed from strings to symbols

--- lib/rails_exception_handler/configuration.rb
+++ lib/rails_exception_handler/configuration.rb
@@ -2,7 +2,7 @@ class RailsExceptionHandler::Configuration
   attr_accessor :environments, :catch_routing_errors, :responses
 
   def initialize
-    @environments = ['production']
+    @environments = [:production]
     @catch_routing_errors = true
     @responses = { '404' => '<h1>Page not found</h1><p>The page you were looking for could not be found.</p>',
                    '500' => '<h1>An error has occurred</h1><p>The error has been logged and will be reviewed by a system administrator.</p>' }
--- spec/exception_handler_test_app/config/initializers/rails_exception_handler.rb
+++ spec/exception_handler_test_app/config/initializers/rails_exception_handler.rb
@@ -1,7 +1,4 @@
 
 RailsExceptionHandler.configure do |config|
-  config.environments = ['test']
-  # config.catch_routing_errors = false
-  config.responses['404'] = "<h1>404</h1><p>Page not found</p>"
-  config.responses['500'] = "<h1>500</h1><p>Internal server error</p>"
+  config.environments = [:test]
 end
