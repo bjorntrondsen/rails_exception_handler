091f8152a8983ebc671cb64e11c16f171adf9a06
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-11 21:19:46 +0200

    option for ignoring crawlers added

--- lib/rails_exception_handler/configuration.rb
+++ lib/rails_exception_handler/configuration.rb
@@ -1,9 +1,10 @@
 class RailsExceptionHandler::Configuration
-  attr_accessor :storage_strategy, :environments, :catch_routing_errors, :responses, :fallback_layout
+  attr_accessor :storage_strategy, :environments, :catch_routing_errors, :ignore_crawlers, :responses, :fallback_layout
 
   def initialize
     @environments = [:production]
     @catch_routing_errors = true
+    @ignore_crawlers = false
     @responses = { '404' => '<h1>Page not found</h1><p>The page you were looking for could not be found.</p>',
                    '500' => '<h1>An error has occurred</h1><p>The error has been logged and will be reviewed by a system administrator.</p>' }
     @fallback_layout = 'application'
@@ -11,5 +12,9 @@ class RailsExceptionHandler::Configuration
 
   def catch_routing_errors?
     @catch_routing_errors
+  end
+
+  def ignore_crawlers?
+    @ignore_crawlers
   end
 end
