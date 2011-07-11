1647aaace2e80469bfae9fdd6a65eb9f77cec982
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-11 21:27:53 +0200

    catch_routing_errors renamed to ignore_routing_errors

--- lib/rails_exception_handler/configuration.rb
+++ lib/rails_exception_handler/configuration.rb
@@ -1,17 +1,17 @@
 class RailsExceptionHandler::Configuration
-  attr_accessor :storage_strategy, :environments, :catch_routing_errors, :ignore_crawlers, :responses, :fallback_layout
+  attr_accessor :storage_strategy, :environments, :ignore_routing_errors, :ignore_crawlers, :responses, :fallback_layout
 
   def initialize
     @environments = [:production]
-    @catch_routing_errors = true
+    @ignore_routing_errors = false
     @ignore_crawlers = false
     @responses = { '404' => '<h1>Page not found</h1><p>The page you were looking for could not be found.</p>',
                    '500' => '<h1>An error has occurred</h1><p>The error has been logged and will be reviewed by a system administrator.</p>' }
     @fallback_layout = 'application'
   end
 
-  def catch_routing_errors?
-    @catch_routing_errors
+  def ignore_routing_errors?
+    @ignore_routing_errors
   end
 
   def ignore_crawlers?
