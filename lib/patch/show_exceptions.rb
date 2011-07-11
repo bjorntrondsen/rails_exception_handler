0cc22f9961dd41481d4d0982e3498ec5faaa5b24
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-11 23:04:41 +0200

    Add exception if the patch is loaded when the exception handler is configured not to take effect

--- lib/patch/show_exceptions.rb
+++ lib/patch/show_exceptions.rb
@@ -3,7 +3,11 @@ module ActionDispatch
   class ShowExceptions
     private
       def render_exception_with_template(env, exception)
-        RailsExceptionHandler::Handler.new(env, exception).handle_exception
+        if(RailsExceptionHandler.configuration.environments.include?(Rails.env.to_sym))
+          RailsExceptionHandler::Handler.new(env, exception).handle_exception
+        else
+          raise "RailsExceptionHandler: This patch should not have been loaded"
+        end
       end
       alias_method_chain :render_exception, :template
   end
