a10fbd80b05c06ebfb376ae304e15c249aca60ad
Sharagoz <contact@sharagoz.com>
2011-07-11 20:20:12 +0200

    added unit tests for the configuration class

---
+++ spec/unit/configuration_spec.rb
@@ -0,0 +1,29 @@
+
+
+describe RailsExceptionHandler::Configuration do
+  describe ".initialize" do
+    before(:each) do
+      @configuration = RailsExceptionHandler::Configuration.new
+    end
+
+    it "should add :production to environments" do
+      @configuration.environments.should == [:production]
+    end
+    
+    it "should set catch_routing_errors to true" do
+      @configuration.catch_routing_errors.should == true
+    end
+
+    it "should set the 404 response" do
+      @configuration.responses['404'].should match(/Page not found/)
+    end
+
+    it "should set the 500 response" do
+      @configuration.responses['500'].should match(/An error has occurred/)
+    end
+
+    it "should set the fallback layout to 'application'" do
+      @configuration.fallback_layout.should == 'application'
+    end
+  end
+end
