0ea5d94c97bafdf50bd90657a1467ac1d57c1a78
Bjørn Trondsen <bjorn@arctodus.no>
2011-07-14 22:35:53 +0200

    filters added to the configuration class

--- lib/rails_exception_handler/configuration.rb
+++ lib/rails_exception_handler/configuration.rb
@@ -1,11 +1,10 @@
 class RailsExceptionHandler::Configuration
-  attr_accessor :storage_strategies, :environments, :ignore_routing_errors, :ignore_crawlers, :responses, :fallback_layout
+  attr_accessor :storage_strategies, :environments, :filters, :responses, :fallback_layout
 
   def initialize
     @environments = [:production]
     @storage_strategies = []
-    @ignore_routing_errors = false
-    @ignore_crawlers = false
+    @filters = []
     @responses = { '404' => '<h1>Page not found</h1><p>The page you were looking for could not be found.</p>',
                    '500' => '<h1>Internal server error</h1><p>The application has encountered an unexpected issue.</p>' }
     @fallback_layout = 'application'
