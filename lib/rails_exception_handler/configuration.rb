792e1aa647519176199d7cde26d1a027f65c328c
Sharagoz <contact@sharagoz.com>
2011-08-19 18:46:54 +0200

    active_record check did not work for :rails_log or :active_record values

--- lib/rails_exception_handler/configuration.rb
+++ lib/rails_exception_handler/configuration.rb
@@ -12,7 +12,7 @@ class RailsExceptionHandler::Configuration
   end
 
   def active_record?
-    @storage_strategies.map { |s| s.has_key?(:active_record) }.inject{ |i,o| i = i || o }
+    @storage_strategies.include?(:active_record)
   end
 
 end
