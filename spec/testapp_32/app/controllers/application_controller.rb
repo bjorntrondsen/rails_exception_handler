fadf3a1e24cc41244453b5ec349460702f228d24
Bjørn Trondsen <bjorn@arctodus.no>
2012-01-25 22:23:38 +0100

    configure testapps

--- spec/testapp_32/app/controllers/application_controller.rb
+++ spec/testapp_32/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    mock = Object.new
+    def mock.login
+      'matz'
+    end
+    return mock
+  end
+
+  def nil_user
+    return nil
+  end
+
 end
