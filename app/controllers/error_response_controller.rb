0cbb7740a3dd5a06aeacf415cf17f1523d53c025
Bjørn Trondsen <bjorn@arctodus.no>
2012-03-10 13:49:07 +0100

    do not load models and controller unless the exception handler is activated

--- app/controllers/error_response_controller.rb
+++ app/controllers/error_response_controller.rb
@@ -1,9 +1,11 @@
-class ErrorResponseController < ApplicationController
-  def index
-    render(:text => @_env['exception_handler.response'], :layout => @_env['exception_handler.layout'])
-  end
+if(RailsExceptionHandler.configuration.activate?)
+  class ErrorResponseController < ApplicationController
+    def index
+      render(:text => @_env['exception_handler.response'], :layout => @_env['exception_handler.layout'])
+    end
 
-  def dummy_action
-    render :nothing => true
+    def dummy_action
+      render :nothing => true
+    end
   end
 end
