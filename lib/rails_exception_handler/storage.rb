97df13b76c2074300bc3b76356605063b150bf32
Sharagoz <contact@sharagoz.com>
2012-04-02 16:10:24 +0200

    fix nested hash problem when using remote_url storage strategy

--- lib/rails_exception_handler/storage.rb
+++ lib/rails_exception_handler/storage.rb
@@ -13,7 +13,35 @@ class RailsExceptionHandler::Storage
 
   def self.remote_url(target, info)
     uri = URI.parse(target)
-    params = {:error_message => info}
+    params = flatten_hash({:error_message => info})
     Net::HTTP::post_form(uri, params)
+  end
+
+  private
+
+  # Credit: Hash flattening technique borrowed from Peter Marklund: http://marklunds.com/articles/one/314
+  def self.flatten_hash(hash, ancestor_names = [])
+    flat_hash = {}
+    hash.each do |k, v|
+      names = Array.new(ancestor_names)
+      names << k
+      if v.is_a?(Hash)
+        flat_hash.merge!(flatten_hash(v, names))
+      else
+        key = flat_hash_key(names)
+        key += "[]" if v.is_a?(Array)
+        flat_hash[key] = v
+      end
+    end
+    flat_hash
+  end
+
+  def self.flat_hash_key(names)
+    names = Array.new(names)
+    name = names.shift.to_s.dup
+    names.each do |n|
+      name << "[#{n}]"
+    end
+    name
   end
 end
