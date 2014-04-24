5538b8b9e39435bc70988e47779222b9239c9828
ExClouds <exclouds@gmail.com>
2014-04-24 13:49:55 +0300

    database name and table name in config
    added option to specify ActiveRecord database name and table name in
    config

--- app/models/rails_exception_handler/active_record/error_message.rb
+++ app/models/rails_exception_handler/active_record/error_message.rb
@@ -1,10 +1,9 @@
-
 class RailsExceptionHandler::ActiveRecord
 
   class ErrorMessage < defined?(ActiveRecord) ? ActiveRecord::Base : Object
-    if(defined?(ActiveRecord) && RailsExceptionHandler.configuration.activate? && RailsExceptionHandler.configuration.active_record?)
-      establish_connection(:exception_database)
-      self.table_name = :error_messages
+    if (defined?(ActiveRecord) && RailsExceptionHandler.configuration.activate? && RailsExceptionHandler.configuration.active_record?)
+      establish_connection(RailsExceptionHandler.configuration.active_record_database)
+      self.table_name = RailsExceptionHandler.configuration.active_record_table
     end
   end
 
