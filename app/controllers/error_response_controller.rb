948b3b7f7f3e169697bed62549fd882aa24d8ad1
Bjørn Trondsen <bjorn@arctodus.no>
2019-10-03 10:32:29 +0200

    Fix auth token issue

--- app/controllers/error_response_controller.rb
+++ app/controllers/error_response_controller.rb
@@ -1,5 +1,13 @@
 if(RailsExceptionHandler.configuration.activate?)
   class ErrorResponseController < ApplicationController
+
+    if Rails::VERSION::MAJOR > 3
+      skip_before_action :verify_authenticity_token
+    else
+      skip_before_filter :verify_authenticity_token
+    end
+
+
     def index
       if Rails::VERSION::MAJOR > 4
         render(:html => request.env['exception_handler.response'].html_safe, :layout => request.env['exception_handler.layout'])
